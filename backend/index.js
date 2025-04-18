69de495afb9200bfc3be5cd7bcc876354353c710
samsingh2h27e <samsingh2592003@gmail.com>
2025-04-18 22:40:07 +0530

    vercel

--- backend/index.js
+++ backend/index.js
@@ -266,6 +266,6 @@ app.post("/updatePassword",async (req,res)=>{
     }
 })
 
-app.listen(3000,()=>{
-    // console.log("Server is running on port 3000");
+app.listen(process.env.PORT,()=>{
+    console.log("Server is running on port "+process.env.PORT);
 });
